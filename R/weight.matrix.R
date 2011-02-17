0c91dfb1b34268bc792fcfcf5518c150dbc70e51
Frank Konietschke <fkoniet@gwdg.de>
2011-02-17 00:00:00 +0000

    version 1.0-1

--- R/weight.matrix.R
+++ R/weight.matrix.R
@@ -1,7 +1,7 @@
-"weight.matrix" <-
-function(n,type = c("Tukey","AVE","Dunnett", "Sequen",
-                         "Changepoint", "Marcus", 
-                          "McDermott", "Williams"), base = 1) {
+weight.matrix <-
+function(n,type = c("UserDefined","Tukey","AVE","Dunnett", "Sequen",
+                         "Changepoint", "Marcus",
+                          "McDermott", "Williams", "UmbrellaWilliams"), base = 1, contrast.matrix=NULL) {
 multcomp <- require(multcomp, quietly = TRUE)
 a  <- length (n)
 
@@ -11,14 +11,69 @@ tmp <- expand.grid(1:a, 1:a)
         vi <- tmp[[2]][ind]
         vj <- tmp[[1]][ind]
 
+
 type <- match.arg(type)
-switch (type, AVE = {
+
+switch (type,
+UserDefined = {
+if (is.null(contrast.matrix)){stop("Choose a contrast or give a contrast matrix by using 'contrast.matrix = matrix'")}
+ #if(any(abs(contrast.matrix)>1)){stop("The contrast weights must be between 0 and 1!")}
+ if (ncol(contrast.matrix)!=a){stop("The contrast matrix has more or less columns than samples!")}
+ nc<-nrow(contrast.matrix)
+
+for (i in 1:nc){
+places_pos<-(contrast.matrix[i,]>0)
+places_neg<-(contrast.matrix[i,]<0)
+sum_pos<-sum(contrast.matrix[i,][places_pos])
+sum_neg<-sum(contrast.matrix[i,][places_neg])
+if (abs(sum_neg)!= sum(sum_pos)) {
+ stop(" Wrong contrast matrix!The sum of negative and positive weights must be equal") }
+
+for ( j in 1:a){
+
+if (contrast.matrix[i,j]<0){ contrast.matrix[i,j]<-contrast.matrix[i,j]/abs(sum_neg)}
+if (contrast.matrix[i,j]>0){ contrast.matrix[i,j]<-contrast.matrix[i,j]/sum_pos}
+
+}
+ }
+n.row <- nrow(contrast.matrix)
+
+
+ch <- contrast.matrix
+w<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help2<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help1<-matrix(0,nrow = n.row , ncol=n.col )
+for (i in 1:n.row){
+for (j in 1:n.col){
+
+help <- c(rep(i,n.col))
+a <- help[j]
+b <- vi[j]
+d <- vj[j]
+
+w[i,j] <- ch[a,b] *ch[a,d]
+
+if (w[i,j]>0){w[i,j]<-0}
+if (ch[a,b] > 0 && ch[a,d] < 0) {weight.help2[i,j]<-1}
+if(ch[a,b] < 0 && ch[a,d] > 0){weight.help1[i,j] <-1}
+}
+}
+w1<-w*weight.help1
+w2<-w*weight.help2
+w<--1*cbind(w1,w2)
+w
+
+},
+
+AVE = {
 
 
 n.row <- a
 
 ch <- contrMat(n, type="AVE")
 w<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help2<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help1<-matrix(0,nrow = n.row , ncol=n.col )
 for (i in 1:n.row){
 for (j in 1:n.col){
 
@@ -30,25 +85,28 @@ d <- vj[j]
 w[i,j] <- ch[a,b] *ch[a,d]
 
 if (w[i,j]>0){w[i,j]<-0}
-
+if (ch[a,b] > 0 && ch[a,d] < 0) {weight.help2[i,j]<-1}
+if(ch[a,b] < 0 && ch[a,d] > 0){weight.help1[i,j] <-1}
 }
 }
-w<--1*w
+w1<-w*weight.help1
+w2<-w*weight.help2
+w<--1*cbind(w1,w2)
 w
 },
 
-Changepoint = { 
+Changepoint = {
 
 n.row <- a - 1
 
 ch <- contrMat(n, type = "Changepoint")
 w<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help2<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help1<-matrix(0,nrow = n.row , ncol=n.col )
 for (i in 1:n.row){
 for (j in 1:n.col){
 
 help <- c(rep(i,n.col))
-hilf2 <- vi
-hilf4 <- vj
 a <- help[j]
 b <- vi[j]
 d <- vj[j]
@@ -56,25 +114,28 @@ d <- vj[j]
 w[i,j] <- ch[a,b] *ch[a,d]
 
 if (w[i,j]>0){w[i,j]<-0}
-
+if (ch[a,b] > 0 && ch[a,d] < 0) {weight.help2[i,j]<-1}
+if(ch[a,b] < 0 && ch[a,d] > 0){weight.help1[i,j] <-1}
 }
 }
-w<--1*w
+w1<-w*weight.help1
+w2<-w*weight.help2
+w<--1*cbind(w1,w2)
 w
 },
 
-Dunnett = {   
+Dunnett = {
 
 n.row <- a - 1
 
-ch <- contrMat(n, type = "Dunnett", base=base)
+ch <- contrMat(n, type = "Dunnett", base = base)
 w<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help2<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help1<-matrix(0,nrow = n.row , ncol=n.col )
 for (i in 1:n.row){
 for (j in 1:n.col){
 
 help <- c(rep(i,n.col))
-hilf2 <- vi
-hilf4 <- vj
 a <- help[j]
 b <- vi[j]
 d <- vj[j]
@@ -82,10 +143,13 @@ d <- vj[j]
 w[i,j] <- ch[a,b] *ch[a,d]
 
 if (w[i,j]>0){w[i,j]<-0}
-
+if (ch[a,b] > 0 && ch[a,d] < 0) {weight.help2[i,j]<-1}
+if(ch[a,b] < 0 && ch[a,d] > 0){weight.help1[i,j] <-1}
 }
 }
-w<--1*w
+w1<-w*weight.help1
+w2<-w*weight.help2
+w<--1*cbind(w1,w2)
 w
 },
 
@@ -95,12 +159,12 @@ n.row <- a - 1
 
 ch <- contrMat(n, type = "Sequen", base)
 w<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help2<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help1<-matrix(0,nrow = n.row , ncol=n.col )
 for (i in 1:n.row){
 for (j in 1:n.col){
 
 help <- c(rep(i,n.col))
-hilf2 <- vi
-hilf4 <- vj
 a <- help[j]
 b <- vi[j]
 d <- vj[j]
@@ -108,10 +172,13 @@ d <- vj[j]
 w[i,j] <- ch[a,b] *ch[a,d]
 
 if (w[i,j]>0){w[i,j]<-0}
-
+if (ch[a,b] > 0 && ch[a,d] < 0) {weight.help2[i,j]<-1}
+if(ch[a,b] < 0 && ch[a,d] > 0){weight.help1[i,j] <-1}
 }
 }
-w<--1*w
+w1<-w*weight.help1
+w2<-w*weight.help2
+w<--1*cbind(w1,w2)
 w
 },
 
@@ -121,12 +188,12 @@ n.row <- a*(a-1)/2
 
 ch <- contrMat(n, type = "Marcus", base)
 w<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help2<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help1<-matrix(0,nrow = n.row , ncol=n.col )
 for (i in 1:n.row){
 for (j in 1:n.col){
 
 help <- c(rep(i,n.col))
-hilf2 <- vi
-hilf4 <- vj
 a <- help[j]
 b <- vi[j]
 d <- vj[j]
@@ -134,24 +201,28 @@ d <- vj[j]
 w[i,j] <- ch[a,b] *ch[a,d]
 
 if (w[i,j]>0){w[i,j]<-0}
-
+if (ch[a,b] > 0 && ch[a,d] < 0) {weight.help2[i,j]<-1}
+if(ch[a,b] < 0 && ch[a,d] > 0){weight.help1[i,j] <-1}
 }
 }
-w<--1*w
+w1<-w*weight.help1
+w2<-w*weight.help2
+w<--1*cbind(w1,w2)
 w
 },
 
-McDermott = { 
+McDermott = {
 
-n.row <- a -1 
+n.row <- a -1
 
 ch <- contrMat(n, type = "McDermott", base)
-w <- matrix(0,nrow = n.row , ncol=n.col )
+w<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help2<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help1<-matrix(0,nrow = n.row , ncol=n.col )
 for (i in 1:n.row){
 for (j in 1:n.col){
 
 help <- c(rep(i,n.col))
-
 a <- help[j]
 b <- vi[j]
 d <- vj[j]
@@ -159,10 +230,13 @@ d <- vj[j]
 w[i,j] <- ch[a,b] *ch[a,d]
 
 if (w[i,j]>0){w[i,j]<-0}
-
+if (ch[a,b] > 0 && ch[a,d] < 0) {weight.help2[i,j]<-1}
+if(ch[a,b] < 0 && ch[a,d] > 0){weight.help1[i,j] <-1}
 }
 }
-w<--1*w
+w1<-w*weight.help1
+w2<-w*weight.help2
+w<--1*cbind(w1,w2)
 w
 },
 
@@ -172,12 +246,12 @@ n.row <- a - 1
 
 ch <- contrMat(n, type = "Williams")
 w<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help2<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help1<-matrix(0,nrow = n.row , ncol=n.col )
 for (i in 1:n.row){
 for (j in 1:n.col){
 
 help <- c(rep(i,n.col))
-hilf2 <- vi
-hilf4 <- vj
 a <- help[j]
 b <- vi[j]
 d <- vj[j]
@@ -185,10 +259,13 @@ d <- vj[j]
 w[i,j] <- ch[a,b] *ch[a,d]
 
 if (w[i,j]>0){w[i,j]<-0}
-
+if (ch[a,b] > 0 && ch[a,d] < 0) {weight.help2[i,j]<-1}
+if(ch[a,b] < 0 && ch[a,d] > 0){weight.help1[i,j] <-1}
 }
 }
-w<--1*w
+w1<-w*weight.help1
+w2<-w*weight.help2
+w<--1*cbind(w1,w2)
 w
 } ,
 
@@ -198,13 +275,13 @@ Tukey = {
 n.row <- a*(a-1)/2
 
 ch <- contrMat(n, type = "Tukey", base)
-w <- matrix(0,nrow = n.row , ncol=n.col )
+w<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help2<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help1<-matrix(0,nrow = n.row , ncol=n.col )
 for (i in 1:n.row){
 for (j in 1:n.col){
 
 help <- c(rep(i,n.col))
-hilf2 <- vi
-hilf4 <- vj
 a <- help[j]
 b <- vi[j]
 d <- vj[j]
@@ -212,19 +289,47 @@ d <- vj[j]
 w[i,j] <- ch[a,b] *ch[a,d]
 
 if (w[i,j]>0){w[i,j]<-0}
-
+if (ch[a,b] > 0 && ch[a,d] < 0) {weight.help2[i,j]<-1}
+if(ch[a,b] < 0 && ch[a,d] > 0){weight.help1[i,j] <-1}
 }
 }
-w<--1*w
-
-}
-
-
-
-
-)
-rownames(w)<-paste("C",1:n.row)
-colnames(w) <- 1:n.col
+w1<-w*weight.help1
+w2<-w*weight.help2
+w<--1*cbind(w1,w2)
 w
+},
+UmbrellaWilliams = {
+
+n.row <- a*(a-1)/2
+
+ch <- contrMat(n, type = "UmbrellaWilliams", base)
+w<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help2<-matrix(0,nrow = n.row , ncol=n.col )
+weight.help1<-matrix(0,nrow = n.row , ncol=n.col )
+for (i in 1:n.row){
+for (j in 1:n.col){
+
+help <- c(rep(i,n.col))
+a <- help[j]
+b <- vi[j]
+d <- vj[j]
+
+w[i,j] <- ch[a,b] *ch[a,d]
+
+if (w[i,j]>0){w[i,j]<-0}
+if (ch[a,b] > 0 && ch[a,d] < 0) {weight.help2[i,j]<-1}
+if(ch[a,b] < 0 && ch[a,d] > 0){weight.help1[i,j] <-1}
+}
+}
+w1<-w*weight.help1
+w2<-w*weight.help2
+w<--1*cbind(w1,w2)
+w
+}
+)
+ch.out <- matrix(c(ch),nrow=n.row)
+
+result <- list(weight.matrix = w, weight.help1 = weight.help1,weight.help2 = weight.help2,contrast.matrix = ch.out)
+result
 }
 
